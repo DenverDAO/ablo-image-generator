b9a1dd8afe7c412a1029b8d768c2f9b17e72a6a9
proofoftom <tommy.cox@protonmail.com>
2025-01-28 20:17:14 -0700

    feat: auto-open browser when finished building

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,3 @@
 export HUGGINGFACE_ACCESS_TOKEN=your_huggingface_access_token_here
 docker build -t hf-text-to-image . && docker run -d --name my-image-wizard -p 7860:7860 -e PROD=false -e HUGGINGFACE_ACCESS_TOKEN=$HUGGINGFACE_ACCESS_TOKEN hf-text-to-image
+open http://localhost:7860
